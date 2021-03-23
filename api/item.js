f60dd2bd42b429dabf8c3744054509604ea8a0cb
hmarkarian <h.markarian@k-zam.com>
2021-03-23 17:24:15 +0100

    nodemon + getById and getAll

--- api/item.js
+++ api/item.js
@@ -1,6 +1,19 @@
 module.exports = (app, service) => {
     app.get("/item", async (req, res) => {
-        res.json({"hello":"itemApi"})
-        //res.json(await service.dao.getAll(req.user))
+        res.json(await service.dao.getAll())
     })
+    app.get("/item/:id", async (req, res) => {
+        try {
+            const item = res.json(await service.dao.getById(req.params.id))
+            if(item === undefined){
+                return res.status(404).end
+            }
+            return res.json(item)
+        }catch (e) {
+            res.status(400).end
+        }
+    })
+    // app.get("/item/list/:id_list", async (req, res) => {
+    //     res.json(await service.dao.getAll())
+    // })
 }
--- api/list.js
+++ api/list.js
@@ -1,6 +1,17 @@
 module.exports = (app, service) => {
     app.get("/list", async (req, res) => {
-        res.json({"hello":"listApi"})
-        //res.json(await service.dao.getAll(req.user))
+        res.json(await service.dao.getAll())
+    })
+
+    app.get("/list/:id", async (req, res) => {
+        try {
+            const list = res.json(await service.dao.getById(req.params.id))
+            if(list === undefined){
+                return res.status(404).end
+            }
+            return res.json(list)
+        }catch (e) {
+            res.status(400).end
+        }
     })
 }
